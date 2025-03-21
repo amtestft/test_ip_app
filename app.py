2c3432bef0989a4a500f2f947db846fbf17e468a
amtestft <a.montinaro@fortop.it>
2025-03-21 11:04:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from category_encoders import TargetEncoder
 
-import base64
+from processor import MultiColumnLabelEncoder, ClickDataPreprocessor
 
 MAPPING = {
     "0": "@", "1": "A", "2": "B", "3": "C", "4": "D",
